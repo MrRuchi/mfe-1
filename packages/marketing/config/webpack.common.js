f9ef03677cb2a41b91e627d467f70ff70224ace3
MrRuchi <ruchirasamarsinghe94@gmail.com>
2023-01-21 07:52:39 +0530

    initial commit

---
+++ packages/marketing/config/webpack.common.js
@@ -0,0 +1,20 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react", "@babel/preset-env"],
+            Plugins: ["@babel/plugin-transform-runtime"],
+          },
+        },
+      },
+    ],
+  },
+};
+
+//test: /\.m?js$/,  Whenever import a file with extension of .mjs or .js will be processed by babel
+//exclude: /node_modules/, Do not try to run this babel thing on any file out of our node modules directory
